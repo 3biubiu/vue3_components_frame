8600a546f5298b8362c9f6c6f13c471a195c980a
zhangheng <zhangheng@zhaoshang.net>
2023-04-12 18:08:25 +0800

    小改

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -66,12 +66,12 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        DefineOptions(),
         dts({
             entryRoot: "./src",
             outputDir: ["../biu/es/src", "../biu/lib/src"],
             //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
             tsConfigFilePath: "../../tsconfig.json",
         }),
-        DefineOptions(),
     ],
 })
