66ebafd9ca91c42be7d91129c2f180cf7af8e14e
3biubiu <2227358943@qq.com>
2023-04-20 00:08:19 +0800

    完成基础的组件库打包,完成按需导入与样式打包, 完善注释

--- packages/components/script/build/index.ts
+++ packages/components/script/build/index.ts
@@ -1,22 +1,34 @@
 import delPath from "../utils/delpath";
-import { series, parallel,src, dest } from "gulp";
+import run from "../utils/run"
+import { series, parallel, src, dest } from "gulp";
 import { componentPath, pkgPath } from "../utils/paths";// 删除上次构建的文件
 const less = require("gulp-less");
 // 自动加兼容前缀
 const autoprefixer = require("gulp-autoprefixer");
+// 删除之前的dist
 export const removeDist = () => {
     return delPath(`${pkgPath}/biu`)
 }
 
 // 打包样式
+
 export const buildStyle = () => {
     // // src() 创建一个流，用于从文件系统读取 Vinyl 对象。
     return src(`${componentPath}/src/**/style/**.less`)
-    // pipe 用于衔接文件流 固定数据处理的流程 
-    .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(dest(`${pkgPath}/biu/lib/src`))
-    .pipe(dest(`${pkgPath}/biu/es/src`));
+    // `${componentPath}/src/**/style/**.less` 的base 路径会被解析成 ${componentPath}/src
+        // pipe 用于衔接文件流 固定数据处理的流程 
+        // 处理less
+        .pipe(less())
+        // 自动前缀
+        .pipe(autoprefixer())
+        // dest  写入文件到指定路径 将 ${componentPath}/src 替换为 下面的路径
+        .pipe(dest(`${pkgPath}/biu/lib/src`))
+        .pipe(dest(`${pkgPath}/biu/es/src`));
 }
-
-export default series(async () => removeDist())
+// 打包样式路径处理的答疑, gulp.dest 会对 gulp.src的base路径进行替换处理,会保持之前的相对结构, 所以每个组件的less 文件的相对路径不会发生改变
+//打包组件
+export const buildComponent = async () => {
+    run("pnpm run build", componentPath);
+};
+// 打包组件与打包样式可以同时进行
+export default series(async () => removeDist(), parallel(async () => buildStyle(), async () => buildComponent()))
---
+++ packages/components/script/utils/run.ts
@@ -0,0 +1,21 @@
+// node 提供的子进程模块 实现多个进程同时执行, 进程之间通过消息进行通信
+// spawn 函数在新的进程中启动一个命令，我们可以通过新的进程向命令传递任何参数。
+import { rejects } from "assert";
+import { spawn } from "child_process";
+import { resolve } from "path";
+
+export default async (command: string, path: string) => {
+    //cmd表示命令，args代表参数，如 rm -rf  rm就是命令，-rf就为参数
+
+    const [cmd, ...args] = command.split(' ')
+
+    return new Promise((resolve, reject) => {
+        const app = spawn(cmd, args, {
+            cwd: path, //执行命令的路径
+            stdio: 'inherit', // //输出共享给父进程
+            shell: true,  //mac不需要开启，windows下git base需要开启支持
+        });
+        //执行完毕关闭并resolve
+        app.on("close", resolve);
+    })
+}
